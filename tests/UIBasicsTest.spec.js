70a9885fdc14b3ac26af18e54ce1c56b8b65a12f
lii-p <liannapyman@gmail.com>
2023-05-04 18:47:11 +0930

    Makes headless true so tests can pass remotely

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
-test.only("Browser Context Playwright Test", async ({ browser }) => {
+test("Browser Context Playwright Test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
