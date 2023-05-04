e200e7a08407ff02b902fd012bdbedddb99bd775
lii-p <liannapyman@gmail.com>
2023-05-04 18:29:36 +0930

    Introduction to playwright

---
+++ README.md
@@ -0,0 +1,9 @@
+Hello!
+Welcome to my learnings of Playwright.
+
+You'll find many wonderful tests here,ranging from little beginner tests to more advanced frameworks, etc.
+
+Please, feel free to take a look around. If you wish to run these on your local machine, you must install playwright through npm.
+
+Check out playwright's official documentation to get started:
+https://playwright.dev/docs/intro
---
+++ tests/UIBasicsTest.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require("@playwright/test");
+
+test.only("Browser Context Playwright Test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+  await page.locator("#username").type("rahulshetty");
+  await page.locator("[type='password']").type("learning");
+  await page.locator("#signInBtn").click();
+  const warningMsg = await page.locator("[style*='block']").textContent();
+  console.log(warningMsg);
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+});
+
+test("Page Playwright Test", async ({ page }) => {
+  await page.goto("https://google.com");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
+});
